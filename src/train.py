f41621e5a7312c16b4901637350949859eb3656f
Vaishnavi nerkar <vaishnavinerkar@gmail.com>
2026-08-27 14:41:28 +0530

    feat: add classification report to training script

--- src/train.py
+++ src/train.py
@@ -24,6 +24,7 @@ def evaluate_model(model, X_test, y_test):
     predictions = model.predict(X_test) 
     acc = accuracy_score(y_test, predictions) 
     report = classification_report(y_test, predictions) 
+    print(classification_report(y_test, predictions)) 
     return acc, report 
  
  
